07c2cdf43f5827673894deb039c5cbc88ccd5874
RiddleMajor <artisanmain3451@gmail.com>
2024-10-04 19:51:04 -0400

    character refine ment

--- Scripts/Godot4FPS.gd
+++ Scripts/Godot4FPS.gd
@@ -1,46 +1,57 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3d
+@export var speed := 5.0
+
+var target_camera_y: float
+var original_camera_y: float  # To store the initial position
+
+func _ready():
+	original_camera_y = camera.position.y  # Save the initial Y position
+	target_camera_y = camera.position.y  # Initialize target Y position
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_pressed("ui_cancel"):
+	elif event.is_action_pressed("esc"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			neck.rotate_y(-event.relative.x * 0.01)
 			camera.rotate_x(-event.relative.y * 0.01)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= (gravity * delta) + 0.1
+		
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		target_camera_y -= 0.3
+		await get_tree().create_timer(0.1).timeout
+		target_camera_y = original_camera_y
 		velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed("e") and $"../../Enviroment/StaticBody3D2".Interact==1:
-		DialogueManager.show_dialogue_balloon(load("res://Dialogue/test.dialogue"), "start")
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "foward", "back")
+		
+	camera.position.y = lerp(camera.position.y, target_camera_y, 6.0 * delta)
+	
+	if Input.is_action_pressed("shift"):
+		speed = 6.5
+	if Input.is_action_just_released("shift"):
+		speed = 5
+	
+	var input_dir := Input.get_vector("a", "d", "w", "s")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+
